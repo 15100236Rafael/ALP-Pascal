272eb019d6360eb4def3e50846d4113fb3d41692
Rafael Santos <15100236.ads.fatecsp@gmail.com>
2015-05-26 00:48:18 -0300

    Commiting a combining algorithm

---
+++ combina4.pas
@@ -0,0 +1,15 @@
+{Program to combine an array of 6 numbers in groups of four}
+PROGRAM combina4;
+VAR a, b, c, d, e: INTEGER;
+    seq: ARRAY [1..6] OF INTEGER;
+LABEL START;
+BEGIN
+    writeln('Type 6 numbers');
+    FOR e:=1 TO 6 DO
+        readln(seq[e]);
+    FOR a:=1 TO 3 DO
+        FOR b:=(a+1) TO 4 DO
+            FOR c:=(b+1)TO 5 DO
+                FOR d:=(c+1)TO 6 DO
+                    writeln(seq[a],' ',seq[b],' ',seq[c],' ',seq[d]);
+END.
