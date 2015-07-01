5a9f3fe9edfa1dfa9ede2a53792ffb25c8aad28a
Rafael Santos <15100236Rafae.ads.fatecsp@gmail.com>
2015-07-01 02:22:03 -0300

    Ajusta miudezas do script

--- compila.py
+++ compila.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from subprocess import call
 import shutil
-progName = raw_input("Input sourcecode name:")
+progName = raw_input("Input sourcecode name: ")
 shutil.copy2(progName,"executables")
 call(["fpc", "executables/"+progName])
+call(["rm","executables/"+progName])
