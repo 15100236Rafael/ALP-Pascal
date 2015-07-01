7c37456d4c9da7406008d32a1f059904d30ad6d6
Rafael Santos <15100236Rafae.ads.fatecsp@gmail.com>
2015-07-01 00:46:55 -0300

    Add the sort programa to the repo

--- sort.pas
+++ sort.pas
@@ -1,8 +1,21 @@
 {Sort numbers}
 PROGRAM sortNumbers;
 USES crt;
-VAR j,i,n : INTEGER;
-    cards: ARRAY [1..4] OF INTEGER;
+VAR j,i,n,k,c : INTEGER;
+    cards: ARRAY [1..6] OF INTEGER;
 BEGIN
-
+    Writeln('Type 6 whole numbers:');
+    FOR c:=1 TO 6 DO
+        Read(cards[c]);
+    FOR i:= 1 TO 6 DO
+        FOR j:=i+1 TO 6 DO
+            BEGIN
+                IF cards[i] > cards[j] THEN
+                    BEGIN
+                        k:= cards[i];
+                        cards[i]:= cards[j];
+                        cards[j]:= k;
+                    END;
+            END;
+    Writeln(cards[1],' ',cards[2],' ',cards[3],' ',cards[4],' ',cards[5],' ',cards[6]);
 END.
